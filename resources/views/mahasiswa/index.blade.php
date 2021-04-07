a72c8cf6b300743d4f8cd7f74ece900183d5738a
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2021-04-07 08:11:28 +0700

    fix index mahasiswa

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
+@section('content')
 
-<head>
-    <title>
-    Data Mahasiswa
-    </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-
+beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
+eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" cross origin="anonymous">
+
 <body>
     <div class="container">
         <div class="row">
-            <h1>Data Mahasiswa</h1>
-            <table class="table table-bordered">
+            <h1>Data SIAPAA</h1>
+            <table class="table">
                 <tr>
                     <th>NAMA</th>
                     <th>NIM</th>
@@ -27,6 +25,9 @@
             </table>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-
+beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" cross origin="anonymous"></script>
 </body>
-</html>
+
+@endsection
