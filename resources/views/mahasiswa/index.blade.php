e67fec273751f6c69857ad9a7e263059646cc85e
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2021-04-08 22:53:20 +0700

    delete header

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html>
 
-@extends('layouts.app')
-@section('content')
-
 <head>
     <title>
     </title>
@@ -84,6 +81,5 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
-@endsection
 
 </html>
